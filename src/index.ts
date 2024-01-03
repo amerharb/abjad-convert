9f09c459777766bf25ee78facd2e52f8d7182dc9
Amer Harb <amer.harb@gmail.com>
2024-01-03 11:16:16 +0100

    add both Arabic and Ugaritic alphabet

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello, world!. Abjad Convert')
+import { letters } from "./ugaritic/letters"
+import { letters as arabicLetters } from "./arabic/letters"
+
+console.log('Abjad Convert')
+
+for (const letter of letters) {
+	console.log(letter)
+}
+
+for (const letter of arabicLetters) {
+	console.log(letter)
+}
