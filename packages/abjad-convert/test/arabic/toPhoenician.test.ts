51702b81d80d3dd897bd2d7764747121b9b9a966
Amer Harb <amer.harb@gmail.com>
2024-01-04 12:27:30 +0100

    add jest

---
+++ packages/abjad-convert/test/arabic/toPhoenician.test.ts
@@ -0,0 +1,12 @@
+import { ArabicToPhoenicianConverter } from "../../src/arabic/toPhoenician";
+import { describe, expect, it } from "@jest/globals";
+
+describe('ArabicToPhoenicianConverter', () => {
+	describe('convert()', () => {
+		it('convert ا ب ج into 𐤀𐤟𐤁𐤟𐤂', () => {
+			const converter = new ArabicToPhoenicianConverter();
+			const actual = converter.convert('ا ب ج');
+			expect(actual).toEqual('𐤀𐤟𐤁𐤟𐤂')
+		})
+	})
+})
