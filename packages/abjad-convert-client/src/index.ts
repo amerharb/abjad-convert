716612366aa3217874d94025fbfea7a1cdeadfc5
Amer Harb <amer.harb@gmail.com>
2024-01-04 11:49:12 +0100

    update client

--- packages/abjad-convert-client/src/index.ts
+++ packages/abjad-convert-client/src/index.ts
@@ -11,10 +11,14 @@ console.log(calmConvert(arabic, Abjad.Arabic, Abjad.Phoenician))
 const ugaritic = '𐎀 𐎁 𐎚 𐎂 𐎄 𐎃'
 console.log(`convert Ugaritic text ${ugaritic} to Arabic`)
 console.log(calmConvert(ugaritic, Abjad.Ugaritic, Abjad.Arabic))
+console.log(`convert Ugaritic text ${ugaritic} to Phoenician`)
+console.log(calmConvert(ugaritic, Abjad.Ugaritic, Abjad.Phoenician))
 
 const phoenician = '𐤀 𐤁 𐤂 𐤃 𐤄 𐤅'
 console.log(`convert Phoenician text ${phoenician} to Arabic`)
 console.log(calmConvert(phoenician, Abjad.Phoenician, Abjad.Arabic))
+console.log(`convert Phoenician text ${phoenician} to Ugaritic`)
+console.log(calmConvert(phoenician, Abjad.Phoenician, Abjad.Ugaritic))
 
 function calmConvert(source: string, from: Abjad, to: Abjad): string {
 	try {
