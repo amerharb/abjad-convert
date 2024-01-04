3f4f83368b8ce20feff92970e890f780ee6e3766
Amer Harb <amer.harb@gmail.com>
2024-01-04 10:52:02 +0100

    enhance convert function

--- packages/abjad-convert/src/index.ts
+++ packages/abjad-convert/src/index.ts
@@ -2,6 +2,9 @@ import { getConverter } from './ConverterFactory';
 import { Abjad } from './types';
 
 export function convert(source: string, from: Abjad, to: Abjad): string {
+	if (from === to) {
+		return source;
+	}
 	const converter = getConverter(from, to);
 	return converter.convert(source);
 }
