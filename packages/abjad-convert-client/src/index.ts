23af4bed448d65860f29049f30f1833ce89c057d
Amer Harb <amer.harb@gmail.com>
2024-01-04 09:12:54 +0100

    update client

--- packages/abjad-convert-client/src/index.ts
+++ packages/abjad-convert-client/src/index.ts
@@ -1,4 +1,11 @@
-import { convert } from "abjad-convert/src"
-import { Abjad } from "abjad-convert/src/types"
+import { Abjad, convert } from "abjad-convert/src"
 
-console.log(convert('ا ب ت ج د خ', Abjad.Arabic, Abjad.Ugaritic))
+console.log('Abjad Convert')
+
+const arabic = 'ا ب ت ج د خ'
+console.log(`convert Arabic text ${arabic} to Ugaritic`)
+console.log(convert(arabic, Abjad.Arabic, Abjad.Ugaritic))
+
+const ugaritic = '𐎀 𐎁 𐎚 𐎂 𐎄 𐎃'
+console.log(`convert Ugaritic text ${ugaritic} to Arabic`)
+console.log(convert(ugaritic, Abjad.Ugaritic, Abjad.Arabic))
