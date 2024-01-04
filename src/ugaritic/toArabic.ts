1a7e7ab3f30faf08bfc2af18a13b88077c5d7d3d
Amer Harb <amer.harb@gmail.com>
2024-01-04 05:46:12 +0100

    complete map ug to ar

--- src/ugaritic/toArabic.ts
+++ src/ugaritic/toArabic.ts
@@ -1,17 +1,45 @@
-import { letters as ug} from "./letters"
-import { letters as ar} from "../arabic/letters"
+import { Ug } from "./letters"
+import { Ar } from "../arabic/letters"
 
 const map = new Map<string, string>(
 	[
-		[ug[0], ar[5]],
-		[ug[1], ar[6]],
-		[ug[2], ar[10]],
-		[ug[3], ar[13]],
-		//...
+		[Ug.Alpa, Ar.Alef],
+		[Ug.Beta, Ar.Ba],
+		[Ug.Gamla, Ar.Jeem],
+		[Ug.Kha, Ar.Khaa],
+		[Ug.Delta, Ar.Dal],
+		[Ug.Ho, Ar.Ha],
+		[Ug.Wo, Ar.Waw],
+		[Ug.Zeta, Ar.Zay],
+
+		[Ug.Hota, Ar.H7aa],
+		[Ug.Tet, Ar.TTa],
+		[Ug.Yod, Ar.Yaa],
+		[Ug.Kaf, Ar.Kaf],
+		[Ug.Shin, Ar.Sheen],
+		[Ug.Lamda, Ar.Lam],
+		[Ug.Mem, Ar.Meem],
+		[Ug.Dhal, Ar.Dad],
+
+		[Ug.Nun, Ar.Noon],
+		[Ug.Zu, Ar.Zay],
+		[Ug.Samka, Ar.Seen],
+		[Ug.Ain, Ar.Ayn],
+		[Ug.Pu, Ar.Fa],
+		[Ug.Sade, Ar.Sad],
+		[Ug.Qopa, Ar.Qaf],
+		[Ug.Rasha, Ar.Ra],
+
+		[Ug.Thanna, Ar.THa],
+		[Ug.Ghain, Ar.Ghayn],
+		[Ug.To, Ar.Ta],
+		[Ug.I, Ar.AlefMaksura],
+		[Ug.U, Ar.Waw],
+		[Ug.Ssu, Ar.Sad],
 	]
 );
 
-export function convertUgariticToArabic(ugaritic: string): string {
+export function convert(ugaritic: string): string {
 	let arabic = '';
 	for (const letter of ugaritic) {
 		arabic += map.get(letter) ?? '';
