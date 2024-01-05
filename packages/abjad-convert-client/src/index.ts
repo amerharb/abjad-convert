68bf76edeacba631640cfafdcdb738f10f0866a6
Amer Harb <amer.harb@gmail.com>
2024-01-05 08:03:56 +0100

    add lint to client

--- packages/abjad-convert-client/src/index.ts
+++ packages/abjad-convert-client/src/index.ts
@@ -1,27 +1,27 @@
 import { Abjad, convert } from 'abjad-convert'
 
-console.log('Abjad Convert')
+log('Abjad Convert')
 
 const arabic = 'ا ب ت ج د خ'
-console.log(`convert Arabic text ${arabic} to Ugaritic`)
-console.log(calmConvert(arabic, Abjad.Arabic, Abjad.Ugaritic))
-console.log(`convert Arabic text ${arabic} to Phoenician`)
-console.log(calmConvert(arabic, Abjad.Arabic, Abjad.Phoenician))
+log(`convert Arabic text ${arabic} to Ugaritic`)
+log(calmConvert(arabic, Abjad.Arabic, Abjad.Ugaritic))
+log(`convert Arabic text ${arabic} to Phoenician`)
+log(calmConvert(arabic, Abjad.Arabic, Abjad.Phoenician))
 
 const ugaritic = '𐎀 𐎁 𐎚 𐎂 𐎄 𐎃'
-console.log(`convert Ugaritic text ${ugaritic} to Arabic`)
-console.log(calmConvert(ugaritic, Abjad.Ugaritic, Abjad.Arabic))
-console.log(`convert Ugaritic text ${ugaritic} to Phoenician`)
-console.log(calmConvert(ugaritic, Abjad.Ugaritic, Abjad.Phoenician))
+log(`convert Ugaritic text ${ugaritic} to Arabic`)
+log(calmConvert(ugaritic, Abjad.Ugaritic, Abjad.Arabic))
+log(`convert Ugaritic text ${ugaritic} to Phoenician`)
+log(calmConvert(ugaritic, Abjad.Ugaritic, Abjad.Phoenician))
 
 const phoenician = '𐤀 𐤁 𐤂 𐤃 𐤄 𐤅'
-console.log(`convert Phoenician text ${phoenician} to Arabic`)
-console.log(calmConvert(phoenician, Abjad.Phoenician, Abjad.Arabic))
-console.log(`convert Phoenician text ${phoenician} to Ugaritic`)
-console.log(calmConvert(phoenician, Abjad.Phoenician, Abjad.Ugaritic))
+log(`convert Phoenician text ${phoenician} to Arabic`)
+log(calmConvert(phoenician, Abjad.Phoenician, Abjad.Arabic))
+log(`convert Phoenician text ${phoenician} to Ugaritic`)
+log(calmConvert(phoenician, Abjad.Phoenician, Abjad.Ugaritic))
 
-console.log(calmConvert('مرحبا', Abjad.Arabic, Abjad.Phoenician))
-console.log(calmConvert('مرحبا', Abjad.Arabic, Abjad.Ugaritic))
+log(calmConvert('مرحبا', Abjad.Arabic, Abjad.Phoenician))
+log(calmConvert('مرحبا', Abjad.Arabic, Abjad.Ugaritic))
 
 function calmConvert(source: string, from: Abjad, to: Abjad): string {
 	try {
@@ -29,4 +29,9 @@ function calmConvert(source: string, from: Abjad, to: Abjad): string {
 	} catch (e: any) {
 		return e.message
 	}
+}
+
+function log(message: string): void {
+	// eslint-disable-next-line no-console
+	console.log(message)
 }
