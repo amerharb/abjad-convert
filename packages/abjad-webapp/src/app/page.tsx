9a51683f4cbcffc743c3105bf586c89bdc22c745
Amer Harb <amer.harb@gmail.com>
2024-01-06 20:44:49 +0100

    add dropdown boxes

--- packages/abjad-webapp/src/app/page.tsx
+++ packages/abjad-webapp/src/app/page.tsx
@@ -4,6 +4,24 @@ export default function Home() {
 	return (
 		<main>
 			<h1>Abjad</h1>
+			<label htmlFor="fromDropdown">From:</label>
+			<select id="fromDropdown">
+				<option value="0">Arabic</option>
+				<option value="1">Imperial Aramaic</option>
+				<option value="2">Phoenician</option>
+				<option value="3">Syriac</option>
+				<option value="4">Ugaritic</option>
+			</select>
+
+			<label htmlFor="tiDropdown">To:</label>
+			<select id="toDropdown">
+				<option value="0">Arabic</option>
+				<option value="1">Imperial Aramaic</option>
+				<option value="2">Phoenician</option>
+				<option value="3">Syriac</option>
+				<option value="4">Ugaritic</option>
+			</select>
+
 		</main>
 	)
 }
