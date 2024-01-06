91e0b99cadb8ce37af5bd2184871343fb197f99c
Amer Harb <amer.harb@gmail.com>
2024-01-06 14:59:16 +0100

    add from ImperialAramaic

--- packages/abjad-convert/test/index.test.ts
+++ packages/abjad-convert/test/index.test.ts
@@ -3,6 +3,12 @@ import { describe, expect, it } from '@jest/globals'
 
 describe('convert()', () => {
 	describe('from Arabic', () => {
+		describe('to ImperialAramaic', () => {
+			it('convert ا ب ج into 𐡀 𐡁 𐡂', () => {
+				const actual = convert('ا ب ج', Abjad.Arabic, Abjad.ImperialAramaic)
+				expect(actual).toEqual('𐡀 𐡁 𐡂')
+			})
+		})
 		describe('to Phoenician', () => {
 			it('convert ا ب ج into 𐤀𐤟𐤁𐤟𐤂', () => {
 				const actual = convert('ا ب ج', Abjad.Arabic, Abjad.Phoenician)
@@ -18,6 +24,32 @@ describe('convert()', () => {
 		describe('to Ugaritic', () => {
 			it('convert ا ب ج into 𐎀 𐎁 𐎂', () => {
 				const actual = convert('ا ب ج', Abjad.Arabic, Abjad.Ugaritic)
+				expect(actual).toEqual('𐎀 𐎁 𐎂')
+			})
+		})
+	})
+	describe('from ImperialAramaic', () => {
+		describe('to Arabic', () => {
+			it('convert 𐡀 𐡁 𐡂 into ا ب ج', () => {
+				const actual = convert('𐡀 𐡁 𐡂', Abjad.ImperialAramaic, Abjad.Arabic)
+				expect(actual).toEqual('ا ب ج')
+			})
+		})
+		describe('to Phoenician', () => {
+			it('convert 𐡀 𐡁 𐡂 into 𐤀𐤟𐤁𐤟𐤂', () => {
+				const actual = convert('𐡀 𐡁 𐡂', Abjad.ImperialAramaic, Abjad.Phoenician)
+				expect(actual).toEqual('𐤀𐤟𐤁𐤟𐤂')
+			})
+		})
+		describe('to Syriac', () => {
+			it.skip('convert 𐡀 𐡁 𐡂 into ܐ ܒ ܓ', () => {
+				const actual = convert('𐡀 𐡁 𐡂', Abjad.ImperialAramaic, Abjad.Syriac)
+				expect(actual).toEqual('ܐ ܒ ܓ')
+			})
+		})
+		describe('to Ugaritic', () => {
+			it('convert 𐡀 𐡁 𐡂 into 𐎀 𐎁 𐎂', () => {
+				const actual = convert('𐡀 𐡁 𐡂', Abjad.ImperialAramaic, Abjad.Ugaritic)
 				expect(actual).toEqual('𐎀 𐎁 𐎂')
 			})
 		})
