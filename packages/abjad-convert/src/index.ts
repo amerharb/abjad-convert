3b664ca16907d5a2b5fcae717337303d6cef7371
Amer Harb <amer.harb@gmail.com>
2024-01-17 15:50:54 +0100

    export letters of script

--- packages/abjad-convert/src/index.ts
+++ packages/abjad-convert/src/index.ts
@@ -1,5 +1,13 @@
 import { getConverter } from './converterFactory'
 import { Abjad } from './types'
+import { arabic } from './scripts/arabic/letters'
+import { imperialAramaic } from './scripts/imperialAramaic/letters'
+import { phoenician } from './scripts/phoenician/letters'
+import { syriac } from './scripts/syriac/letters'
+import { tifinagh } from './scripts/tifinagh/letters'
+import { ugaritic } from './scripts/ugaritic/letters'
+
+export { Abjad } from './types'
 
 export function convert(source: string, from: Abjad, to: Abjad): string {
 	if (from === to) {
@@ -9,4 +17,21 @@ export function convert(source: string, from: Abjad, to: Abjad): string {
 	return converter.convert(source)
 }
 
-export { Abjad } from './types'
+export function getLetters(script: Abjad): readonly string[] {
+	switch (script) {
+		case Abjad.Arabic:
+			return arabic.letters
+		case Abjad.ImperialAramaic:
+			return imperialAramaic.letters
+		case Abjad.Phoenician:
+			return phoenician.letters
+		case Abjad.Syriac:
+			return syriac.letters
+		case Abjad.Tifinagh:
+			return tifinagh.letters
+		case Abjad.Ugaritic:
+			return ugaritic.letters
+		default:
+			return []
+	}
+}
