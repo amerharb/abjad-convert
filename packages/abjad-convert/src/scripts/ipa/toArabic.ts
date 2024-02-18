38bfab117fce3a9f6b4788da74c1eb1759ad9d9e
Amer Harb <amer.harb@gmail.com>
2024-02-18 08:31:19 +0100

    WiP: add letters and toArabic converter, still need to add actual mapping and letter names

---
+++ packages/abjad-convert/src/scripts/ipa/toArabic.ts
@@ -0,0 +1,24 @@
+import { Ip } from './letters'
+import { Ar } from '../arabic/letters'
+import { IConverter } from '../../IConverter'
+import { Abjad } from '../../types'
+
+export class IpaToArabicConverter implements IConverter {
+	public readonly from = Abjad.OldSouthArabian
+	public readonly to = Abjad.Arabic
+
+	private map = new Map<string, string>(
+		[
+			[' ', ' '],
+			// TODO: add mapping
+		]
+	)
+
+	public convert(text: string): string {
+		let result = ''
+		for (const letter of text) {
+			result += this.map.get(letter) ?? ''
+		}
+		return result
+	}
+}
